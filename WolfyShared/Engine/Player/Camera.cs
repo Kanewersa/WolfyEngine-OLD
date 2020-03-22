6b259bfb0e5f683b9de5e809c1a4a6e43a238a6b
Jacob <kanewers@gmail.com>
2020-03-22 16:32:53 +0100

    Camera Refactoring

--- WolfyShared/Engine/Player/Camera.cs
+++ WolfyShared/Engine/Player/Camera.cs
@@ -26,22 +26,6 @@ namespace WolfyShared.Game
 
         public Rectangle GetVisibleArea()
         {
-            /*var inverseViewMatrix = Matrix.Invert(Transform);
-
-            var tl = Vector2.Transform(Vector2.Zero, inverseViewMatrix);
-            var tr = Vector2.Transform(new Vector2(Bounds.X, 0), inverseViewMatrix);
-            var bl = Vector2.Transform(new Vector2(0, Bounds.Y), inverseViewMatrix);
-            var br = Vector2.Transform(new Vector2(Position.X, Position.Y), inverseViewMatrix);
-
-            var min = new Vector2(
-                MathHelper.Min(tl.X, MathHelper.Min(tr.X, MathHelper.Min(bl.X, br.X))),
-                MathHelper.Min(tl.Y, MathHelper.Min(tr.Y, MathHelper.Min(bl.Y, br.Y))));
-            var max = new Vector2(
-                MathHelper.Max(tl.X, MathHelper.Max(tr.X, MathHelper.Max(bl.X, br.X))),
-                MathHelper.Max(tl.Y, MathHelper.Max(tr.Y, MathHelper.Max(bl.Y, br.Y))));
-
-            return new Rectangle((int)min.X, (int)min.Y, (int)max.X, (int)max.Y);*/
-
             var inverseViewMatrix = Matrix.Invert(Transform);
             var tl = Vector2.Transform(Vector2.Zero, inverseViewMatrix);
             var tr = Vector2.Transform(new Vector2(Bounds.X, 0), inverseViewMatrix);
@@ -66,8 +50,7 @@ namespace WolfyShared.Game
                 max.Y = MapBounds.Y;
             }
 
-            var rect = new Rectangle((int)min.X, (int)min.Y, (int)(max.X - min.X), (int)(max.Y - min.Y));
-            return rect;
+            return new Rectangle((int)min.X, (int)min.Y, (int)(max.X - min.X), (int)(max.Y - min.Y)); ;
         }
 
         public void AdjustZoom(float zoomAmount)
@@ -79,24 +62,6 @@ namespace WolfyShared.Game
                 Zoom = .5f;
             if (Zoom > 2f)
                 Zoom = 2;
-        }
-
-        public void UpdateCamera(Viewport bounds)
-        {
-
-            var cameraMovement = Vector2.Zero;
-            int moveSpeed;
-
-            if (Zoom > .8f)
-                moveSpeed = 15;
-            else if (Zoom < .8f && Zoom >= .65f)
-                moveSpeed = 20;
-            else if (Zoom < .65f && Zoom > .5f)
-                moveSpeed = 25;
-            else if (Zoom <= .5f)
-                moveSpeed = 30;
-            else
-                moveSpeed = 10;
         }
 
         public void FollowTarget(Sprite target)
