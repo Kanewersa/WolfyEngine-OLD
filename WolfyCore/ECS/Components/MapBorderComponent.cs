1b884664b33413159f5f694a7b63618dc5f50ef5
Jacob <kanewers@gmail.com>
2020-09-21 14:18:46 +0200

    Update

---
+++ WolfyCore/ECS/Components/MapBorderComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using ProtoBuf;
+using WolfyECS;
+
+namespace WolfyCore.ECS
+{
+    /// <summary>
+    /// Entities with that component teleport other entities to different map.
+    /// </summary>
+    [ProtoContract]
+    public class MapBorderComponent : EntityComponent
+    {
+        /// <summary>
+        /// The map on which entity is located.
+        /// </summary>
+        public int OriginMap { get; set; }
+
+        /// <summary>
+        /// The map to which entities are teleported.
+        /// </summary>
+        public int TargetMap { get; set; }
+
+        /// <summary>
+        /// The entity coordinates.
+        /// </summary>
+        public Vector2 Origin { get; set; }
+
+        /// <summary>
+        /// The coordinates to which entities are teleported.
+        /// </summary>
+        public Vector2 Target { get; set; }
+
+        /// <summary>
+        /// The direction to move the entity before teleport.
+        /// </summary>
+        public int OriginDirection { get; set; }
+
+        /// <summary>
+        /// The direction to move the entity after teleport.
+        /// </summary>
+        public int TargetDirection { get; set; }
+    }
+}
---
+++ WolfyCore/ECS/Systems/MapBorderSystem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProtoBuf;
+using WolfyECS;
+
+namespace WolfyCore.ECS
+{
+    [ProtoContract]
+    public class MapBorderSystem : EntitySystem
+    {
+        public override void Initialize(GraphicsDevice graphics)
+        {
+            RequireComponent<ActiveComponent>();
+            RequireComponent<MapBorderComponent>();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            IterateEntities(entity =>
+            {
+
+            });
+        }
+    }
+}
